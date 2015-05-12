1a3e38e0e190be4b854402d34a700a914dd14605
adamtootle <adamtootle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 13:30:49 +0000

    tagging 0.9.6
    git-svn-id: https://plugins.svn.wordpress.org/staff-directory/tags/0.9.6@1158822 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- staff-directory.php
+++ staff-directory.php
@@ -3,7 +3,7 @@
 Plugin Name: Staff Directory
 Plugin URI: http://www.89designs.net/2010/01/staff-directory/
 Description: Allows Wordpress to keep track of your staff directory for your website. Good for churches, small companies, etc.
-Version: 0.9.4
+Version: 0.9.6
 Author: Adam Tootle
 Author URI: http://www.adamtootle.com
 */
